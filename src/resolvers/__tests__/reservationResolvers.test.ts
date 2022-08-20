9b63d2092c8482a2079f618ed68803f25943101e
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-08-20 20:21:06 +0200

    mod

--- src/resolvers/__tests__/reservationResolvers.test.ts
+++ src/resolvers/__tests__/reservationResolvers.test.ts
@@ -2,14 +2,13 @@ import { ApolloServer } from "apollo-server-express";
 import { createApolloServer, createMikroORM } from "../../index";
 
 describe("resolvers", () => {
-    // const testCustomer = { name: "bilbo baggins", password: "12345", email: "bilbobaggins@shire.com", username: "Biblo", creditCard: "123456789", age: 150 };
-    // let customerId: number;
+
     let testServer: ApolloServer;
     beforeAll(async () => {
         testServer = await createApolloServer(true);
     });
     afterAll(async () => {
-        // await testServer.stop();
+        await testServer.stop();
     });
 
     test('reserve', async () => {
